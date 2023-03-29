e267902f0789acb27793ee0a58a0a4542b316ce3
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-03-29 17:04:51 -0400

    added udpate funcationality

--- server.js
+++ server.js
@@ -29,7 +29,24 @@ db.once('open', function (){
 app.get('/books', getBook);
 app.delete('/books/:bookID', deleteBook);
 app.post('/books', postBook);
+app.put('/books/:bookID',updateBook );
 
+
+async function updateBook(request,response,next) {
+  try {
+    let id = request.params.bookID;
+
+    let data = request.body; 
+
+    const updatedBook = await Book.findByIdAndUpdate(id,data,{new: true, overwrite: true});
+
+    response.status(200).send(updatedBook);
+  
+  } catch(error) {
+    next(error);
+
+  }
+}
 async function postBook(request,response,next){
 try{
 let createdBook = await Book.create(request.body)
